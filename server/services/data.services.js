117184e2cd50d112260a81135133e0c29bbe49e4
faiyaz032 <faiyazrahman03@gmail.com>
2022-09-29 02:25:58 +0600

    added a comment

--- server/services/data.services.js
+++ server/services/data.services.js
@@ -1,5 +1,6 @@
 import mongoose from 'mongoose';
 
+//TODO: Try to find a way to do it in a more better way..
 export const getCrudsModelData = async modelName => {
   try {
     const { db } = mongoose.connection;
